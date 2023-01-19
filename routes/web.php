f4588f0f4a162435c7f5f65b54a4cb6793dc97f8
Mardon <aslonovmardon1995@gmail.com>
2023-01-19 22:17:08 +0500

    Changed routes

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.auth');
 })->name('auth.auth');
-Route::post('/userAuth',[ShopController::class,'userAuth'])->name('userAuth');
+Route::post('user/auth',[ShopController::class,'userAuth'])->name('userAuth');
 
 // createUser
-Route::get('/userTable',[ShopController::class,'userTable'])->name('userTable');
-Route::get('/userCreate',[ShopController::class,'userCreate'])->name('userCreate');
-Route::post('/createInfo',[ShopController::class,'createInfo'])->name('createInfo');
+Route::get('user/table',[ShopController::class,'userTable'])->name('userTable');
+Route::get('user/create',[ShopController::class,'userCreate'])->name('userCreate');
+Route::post('create/info',[ShopController::class,'createInfo'])->name('createInfo');
 
 // userDelete
-Route::get('/userDelete/{id}',[ShopController::class,'userDelete'])->name('userDelete');
+Route::get('user/delete/{id}',[ShopController::class,'userDelete'])->name('userDelete');
 
 // category
 Route::get('category',[ShopController::class,'category'])->name('category');
-Route::get('categoryCreate',[ShopController::class,'categoryCreate'])->name('categoryCreate');
-Route::get('createCategoryInfo',[ShopController::class,'createCategoryInfo'])->name('createCategoryInfo');
+Route::get('category/create',[ShopController::class,'categoryCreate'])->name('categoryCreate');
+Route::get('createCategory/info',[ShopController::class,'createCategoryInfo'])->name('createCategoryInfo');
 
 
 
