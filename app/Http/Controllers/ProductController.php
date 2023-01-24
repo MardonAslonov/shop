0338b8743f78580ff83693ecda9d45174f4edee0
Mardon <aslonovmardon1995@gmail.com>
2023-01-24 19:02:19 +0500

    Qisqartirish

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,7 @@ class ProductController extends Controller
 
     public function index(Request $request)
     {
+
         $products=Product::where('category_id',$request->category_id)->get();
         return view('products.index',[
             'products'=>$products,
